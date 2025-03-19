428ba5dc006464b4db5c99bf5f5bd38a7185be6f
m.maier <m.maier@htlvb.at>
2025-03-19 09:22:41 +0100

    cancel download feature

--- SimpleVideoDownloader/Form1.cs
+++ SimpleVideoDownloader/Form1.cs
@@ -1,7 +1,12 @@
+using System.Security.Policy;
+using System.Windows.Forms;
+
 namespace SimpleVideoDownloader
 {
     public partial class Form1 : Form
     {
+        private CancellationTokenSource? cts = null;
+
         public Form1()
         {
             InitializeComponent();
@@ -15,11 +20,47 @@ namespace SimpleVideoDownloader
 
         private async void btnDownload_Click(object sender, EventArgs e)
         {
+            if (cts != null)
+            {
+                cts.Cancel();
+                cts = null;
+                btnDownload.Text = "Download starten";
+                return;
+            }
+
             lbl_status.Text = "Video wird heruntergeladen";
-            HttpClient httpClient = new();
-            byte[] videoContent = await httpClient.GetByteArrayAsync(txt_videoUrls.Lines[0]);
-            await Task.Run(() => File.WriteAllBytesAsync("../../../video.mp4", videoContent));
-            lbl_status.Text = "Video wurde heruntergeladen.";
+            btnDownload.Text = "Download abbrechen";
+            txt_videoUrls.ReadOnly = true;
+            try
+            {
+                cts = new CancellationTokenSource();
+                await DownloadVideos(txt_videoUrls.Lines, cts.Token);
+                lbl_status.Text = "Video wurde heruntergeladen.";
+            }
+            catch(OperationCanceledException)
+            {
+                lbl_status.Text = "Download wurde abgebrochen";
+            }
+            catch
+            {
+                lbl_status.Text = "Fehler beim Download des Videos.";
+            }
+            txt_videoUrls.ReadOnly = false;
+        }
+
+        private async Task DownloadVideos(string[] urls, CancellationToken ct)
+        {
+            foreach (var url in urls)
+            {
+                byte[] videoContent = await LoadVideo(url,ct);
+                await File.WriteAllBytesAsync("../.. /../video.mp4", videoContent, ct);
+            }
+        }
+
+        private async Task<byte[]> LoadVideo(string url, CancellationToken ct)
+        {
+            using HttpClient httpClient = new();
+            return await httpClient.GetByteArrayAsync(url, ct);
         }
     }
 }
